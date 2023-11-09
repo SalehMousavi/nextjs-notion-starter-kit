4787568f8e65b27087fdb513127801a7fe6d4665
SalehMousavi <142183541+SalehMousavi@users.noreply.github.com>
2023-11-08 19:59:36 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Portfolio',
+  domain: 'salehmousavi.org',
+  author: 'Saleh Mousavi',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'Saleh Mousavi',
+  linkedin: 'Saleh Mousavi',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
